7d12cbf3a01d6554f746dbbcba060326cb29de52
DaiKenja1318 <22010448@st.phenikaa-uni.edu.vn>
2025-06-26 11:32:09 +0700

    .

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+// Đảm bảo file này không trống và có nội dung đúng
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
 {
-    //
+    use AuthorizesRequests, ValidatesRequests;
 }
